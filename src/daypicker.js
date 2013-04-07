e5bd1d1372a7ea5017a015eac328fd743ad46e96
Matt Sacks <matt.s.sacks@gmail.com>
2013-04-06 17:11:09 -0700

    add an activeIndex property to the DayPicker

--- src/daypicker.js
+++ src/daypicker.js
@@ -2,10 +2,15 @@
 var DayPicker = function() {
   this.template = getTemplate('dayPicker');
   this.element = document.id('dayPicker');
+
+  // default to the last day (today) being active
+  this.activeIndex = data.numDays-1;
+
   return this;
 };
 
 DayPicker.prototype.render = function() {
+  var thiz = this;
   var i = data.numDays;
   var days = [];
   var today = new Date().clearTime();
@@ -18,8 +23,15 @@ DayPicker.prototype.render = function() {
     // an index of days 
     days: days,
 
+    // return the day of the week text
     dayText: function() {
       return Date.days[new Date(this).getDay()];
+    },
+
+    // return an active class for the day if it's the current selected day
+    dayActive: function() {
+      return thiz.data.days[thiz.activeIndex] == +this ?
+        'active' : '';
     }
   };
 
